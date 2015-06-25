be9866cd7afbeeef3ecd61fc028b94ed5dca5826
tolty <kanghs@khu.ac.kr>
2015-06-25 12:05:22 +0900

    Create kilo-0.0.all.sh

---
+++ openstack_script/Scripts/kilo-0.0.all.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# Werite by: Jeon.sungwook 
+# Create Date : 2015-06-02
+# Update Date : 2015-06-02
+#
+# OS : CentOS-7-x86_64 1503-01
+# Node : controller 
+# Text : OPENSTACK INSTALLATION GUIDE FOR RED HAT ENTERPRISE LINUX 7, CENTOS 7, AND FEDORA 21  - KILO
+#
+# Perform script for for the chapter 2. Basic environment
+# 
+# This script is to be installed and run on OpenStack Kilo
+# 
+# Set environment and declare global variables
+# ============================================================================================
+
+
+# =========================================================================
+# 기본적으로 OS Setup 및 Network Setup이 완료되어야만 한다.
+# =========================================================================
+
+# Load Env global variables
+. ./kilo-perform-vars.common.sh
+
+# 필요 Package 설치
+# node : ALL
+# (1) wget
+
+echo '0.0.0.0 =============> yum -y install wget'
+yum -y install wget
+
+# (2) crudini : 2.6.2이후에 만 설치 가능 하므로 뒤로 뺐다.
+# yum -y install crudini
+
+# ======================================================================================================
+# 2. Basic environment
+# 2.5 Network Time Protocol (NTP) SKIP
+
+
+# 2.6 OpenStack packages
+# (1) To configure prerequisites
+echo '2.6.1 ===============> yum -y install wget'
+yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+
+# (2) To enable the OpenStack repository
+echo '2.6.2 ===============> yum -y install  http://rdo.fedorapeople.org/openstack-kilo/rdo-release-kilo.rpm'
+yum -y install  http://rdo.fedorapeople.org/openstack-kilo/rdo-release-kilo.rpm
+
+# (3) To finalize installation
+echo '2.6.3 ===============> yum -y install  openstack-selinux'
+yum -y install  openstack-selinux
+echo '0.0.0.0 =============> yum -y install crudini'
+yum -y install crudini
